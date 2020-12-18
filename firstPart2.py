45973d207b2b8d1387832805157d6a86deddb90d
zabesy <adam-nord@hotmail.com>
2020-12-18 23:29:39 +0100

    Day 1 Part 2

---
+++ firstPart2.py
@@ -0,0 +1,10 @@
+
+inputString = open("firstInput.txt").read().split("\n")
+inputInt = [int(every) for every in inputString] 
+
+for i in range(len(inputInt)):
+    for j in range(i+1,len(inputInt)):
+        for ij in range(j+1,len(inputInt)):
+            if(inputInt[i] + inputInt[j] + inputInt[ij] == 2020):
+                print(str(inputInt[i] * inputInt[j] * inputInt[ij]))
+                break
