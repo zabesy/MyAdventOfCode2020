a36200f7b499c5907b1fa828a8a3e0ddef81a379
zabesy <adam-nord@hotmail.com>
2020-12-18 22:02:47 +0100

    update

--- first.py
+++ first.py
@@ -5,6 +5,5 @@ inputInt = [int(every) for every in inputString]
 for i in range(len(inputInt)):
     for j in range(i+1,len(inputInt)):
         if(inputInt[i] + inputInt[j] == 2020):
-            print("i = " + str(i) + "\n" + "j = " + str(j))
-            print("WE GOT IT!!" + "\n" + str(inputInt[i] * inputInt[j]))
+            print(str(inputInt[i] * inputInt[j]))
             break
