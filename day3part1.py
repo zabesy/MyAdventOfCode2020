73d79f9fe28f19ff3d1ddecc458267b60312b68c
zabesy <adam-nord@hotmail.com>
2020-12-19 01:40:34 +0100

    now third:)

---
+++ day3part1.py
@@ -0,0 +1,21 @@
+
+listan = open("day3input.txt").read().split('\n')
+tree = 0
+i=0
+j=0
+character = '#'
+
+
+while i < (len(listan)):
+    if j >= (len(listan[0])):
+        j = j - (len(listan[0])
+        
+    print(listan)
+
+    if listan[i][j] == character:
+        tree=+1
+
+    i=+1
+    j=+3
+
+
