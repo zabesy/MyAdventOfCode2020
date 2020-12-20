cf663f65c7f83d5041d2136b6af60faacb56e830
zabesy <adam-nord@hotmail.com>
2020-12-20 14:39:05 +0100

    day 7, part2 not completely solved

--- day7/day7part2.py
+++ day7/day7part2.py
@@ -1,47 +1,33 @@
-
 def recur(val):
-    global total
-    global recursedStrings
-    for values in newDict:
-        if values not in recursedStrings:
-            for values2 in newDict[values]:
-                if values2 is None:
-                    continue
-                if val == values2[0]:
-                    recur(values2)
-                    total *= values2[1]
-            recursedStrings.append(values)
+    total = 0
+    for value in newDict[val]:
+        if value is not None:
+            ret = recur(value[0])
+            total += ret * value[1]
+        else:
+            total += 1
+    return total
 
-                    
-            
 
-theList = open("day7input.txt","r").read().split('\n')
-total = 1
-actualTotal = 0
-iteration = 0
-
-recursedStrings = []
+theList = open("day7input.txt", "r").read().split('\n')
+total = 0
 
 newDict = {}
+
 for line in theList:
     stringArray = line.split(' ')
 
     if len(stringArray) == 7:
-        newDict.setdefault((stringArray[0] + " " + stringArray[1]),[]).append(None)
+        newDict.setdefault((stringArray[0] + " " + stringArray[1]), []).append(None)
         continue
 
-    for i in range(4, len(stringArray),4):
-        newList = [(stringArray[i+1] + " " + stringArray[i+2]),int(stringArray[i])]
-        newDict.setdefault((stringArray[0] + " " + stringArray[1]),[]).append(newList)
+    for i in range(4, len(stringArray), 4):
+        newList = [(stringArray[i + 1] + " " + stringArray[i + 2]), int(stringArray[i])]
+        newDict.setdefault((stringArray[0] + " " + stringArray[1]), []).append(newList)
 
 
-for values in newDict:
-    for values2 in newDict[values]:
-        if values2 is None:
-            continue
-        if "shiny gold" == values2[0]:
-            recur(values)
-            total *= values2[1]
-            recursedStrings = []
+for value in newDict["shiny gold"]:
+    ret = recur(value[0])
+    total += ret * value[1]
 
 print(total)
